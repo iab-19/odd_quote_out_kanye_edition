dd6c87127bffcb98209034d834297bc38de87085
Jacob Medina <jacobmedina1618@gmail.com>
2023-06-26 23:21:16 -0400

    added functionality to fetch random Kanye and other quotes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,107 @@
-function init() {
-    fetch('https://api.kanye.rest')
+const numberOfQuestions = 3;
+const numberOfQuotes = 3;
+const quoteCharacterCap = 80;
+
+var otherQuotes = [];  // contains the normal quotes that will be used in the quiz
+var kanyeQuotes = [];  // contains the Kanye West quotes that will be used in the quiz
+
+
+class Quote {
+    constructor(text, author) {
+        this.text = text;
+        this.author = author;
+    }
+}
+
+
+// returns true if a quote is not already in given array
+function quoteNotIncluded(quoteChecked, quotesArray) {
+    if (quotesArray.length === 0) { return true; }
+
+    for (quote of quotesArray) {
+        if (quoteChecked === quote.text) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+// filter out quotes that exceed character maximum, also given an array of quotes to not include
+function filterQuotes(quotes, dontIncludeArray) {
+    const filteredQuotes = [];
+
+    for (const quote of quotes) {
+        if (quote.quoteText.length <= quoteCharacterCap && quoteNotIncluded(quote.quoteText, dontIncludeArray)) { 
+            const quoteObject = new Quote(quote.quoteText, quote.quoteAuthor);
+            filteredQuotes.push(quoteObject);
+        }
+    }
+
+    return filteredQuotes;
+}
+
+
+// filters a single quote given a Quote and array of quotes to not include
+function filterQuote(quote, dontIncludeArray) {
+    return filterQuotes([{
+        quoteText: quote.text,
+        quoteAuthor: quote.author
+    }], dontIncludeArray);
+}
+
+
+function fetchRandomQuotes() {
+    const count = 10;
+    fetch(`https://quote-garden.onrender.com/api/v3/quotes/random?count=${count}`)
         .then(function(response) {
-            console.log(response);
             return response.json();
         })
         .then(function(data) {
-            console.log(data);
+            // console.log(data);
+            otherQuotes = otherQuotes.concat(filterQuotes(data.data, otherQuotes));
 
+            // fetch more quotes if there aren't enough to fill all questions
+            if (otherQuotes.length < numberOfQuotes * numberOfQuestions) { 
+                fetchRandomQuotes();
+            }
+
+            else {
+                console.log(otherQuotes);
+            }
         });
 }
+
+
+function fetchKanyeQuotes() {
+    fetch('https://api.kanye.rest')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            const quote = data.quote;
+
+            const quoteObject = new Quote(quote, 'Kanye West');
+            kanyeQuotes = kanyeQuotes.concat(filterQuote(quoteObject, kanyeQuotes));
+            
+            
+            // fetch more quotes if there aren't enough to fill all questions
+            if (kanyeQuotes.length < numberOfQuestions) { 
+                fetchKanyeQuotes();
+            }
+
+            else {
+                console.log(kanyeQuotes);
+            }
+        });
+}
+
+
+function init() {
+    fetchRandomQuotes();
+    fetchKanyeQuotes();
+}
+
 
 $(init);
