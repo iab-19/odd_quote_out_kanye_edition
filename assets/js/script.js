5d3fed3c727ef3544378464965dc99b170537e06
Jacob Medina <jacobmedina1618@gmail.com>
2023-06-28 14:44:58 -0400

    fetch quotes ansynchronously and create a randomized question set

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
-const numberOfQuestions = 3;
-const numberOfQuotes = 3;
-const quoteCharacterCap = 80;
+const numberOfQuestions = 3;  // number of questions per quiz
+const numberOfOtherQuotes = 3;  // number of other (non-Kanye) quotes per question
+const quoteCharacterCap = 80;  // maximum number of characters any quote can have (for presentation)
+
+var currentQuestion = 0;  // keeps track of which question number the player is on
+
+var questionLoadInterval;
+const waitForQuestionLoadTime = 100;  // number of milliseconds to wait before checking if quotes are loaded
 
 var otherQuotes = [];  // contains the normal quotes that will be used in the quiz
 var kanyeQuotes = [];  // contains the Kanye West quotes that will be used in the quiz
@@ -14,17 +19,17 @@ class Quote {
 }
 
 
-// returns true if a quote is not already in given array
+// returns a random integer from 0 to max
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+// returns true if a quote is NOT already in given array
 function quoteNotIncluded(quoteChecked, quotesArray) {
     if (quotesArray.length === 0) { return true; }
 
-    for (quote of quotesArray) {
-        if (quoteChecked === quote.text) {
-            return false;
-        }
-    }
-
-    return true;
+    return quotesArray.find(quote => quote.text === quoteChecked) === undefined;
 }
 
 
@@ -52,7 +57,8 @@ function filterQuote(quote, dontIncludeArray) {
 }
 
 
-function fetchRandomQuotes() {
+// fetches all non-Kanye quotes needed for an entire quiz
+function fetchOtherQuotes() {
     const count = 10;
     fetch(`https://quote-garden.onrender.com/api/v3/quotes/random?count=${count}`)
         .then(function(response) {
@@ -63,44 +69,144 @@ function fetchRandomQuotes() {
             otherQuotes = otherQuotes.concat(filterQuotes(data.data, otherQuotes));
 
             // fetch more quotes if there aren't enough to fill all questions
-            if (otherQuotes.length < numberOfQuotes * numberOfQuestions) { 
-                fetchRandomQuotes();
+            if (otherQuotes.length < numberOfOtherQuotes * (numberOfQuestions - currentQuestion)) { 
+                fetchOtherQuotes();
             }
 
             else {
-                console.log(otherQuotes);
+                console.log("Other Quotes:", otherQuotes);
             }
         });
 }
 
 
+// fetches all Kanye quotes needed for an entire quiz
 function fetchKanyeQuotes() {
     fetch('https://api.kanye.rest')
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
-            const quote = data.quote;
+            var quote = data.quote;
 
+            // add punctuation to end of quote so it fits in better
+            const endsWithPunctuation = quote.endsWith('.') || quote.endsWith('!') || quote.endsWith('!');
+            if (!endsWithPunctuation) { quote = quote + "."; }
+
+            // make quote object and add it to Kanye quotes array
             const quoteObject = new Quote(quote, 'Kanye West');
             kanyeQuotes = kanyeQuotes.concat(filterQuote(quoteObject, kanyeQuotes));
             
             
             // fetch more quotes if there aren't enough to fill all questions
-            if (kanyeQuotes.length < numberOfQuestions) { 
+            if (kanyeQuotes.length < numberOfQuestions - currentQuestion) {
                 fetchKanyeQuotes();
             }
 
             else {
-                console.log(kanyeQuotes);
+                console.log("Kanye Quotes:", kanyeQuotes);
             }
         });
 }
 
 
-function init() {
-    fetchRandomQuotes();
+// returns an array of a new question set, removing used quotes from their respective arrays
+function generateQuestionSetArray() {
+    // get and delete a random Kanye quote
+    const kanyeQuoteIndex = getRandomInt(kanyeQuotes.length);
+    const kanyeQuote = kanyeQuotes[kanyeQuoteIndex];
+    kanyeQuotes.splice(kanyeQuoteIndex, 1);
+
+    // add Kanye quote to question set array
+    const questionSet = [kanyeQuote];
+
+    // get and delete a few other quotes
+    for (var i = 0; i < numberOfOtherQuotes; i++) {
+        const otherQuoteIndex = getRandomInt(otherQuotes.length);
+        const otherQuote = otherQuotes[otherQuoteIndex];
+        otherQuotes.splice(otherQuoteIndex, 1);
+
+        questionSet.push(otherQuote);
+    }
+    
+    // scramble question set array
+    const scrambledQuestionSet = [];
+    while (questionSet.length > 0) {
+        const randomIndex = getRandomInt(questionSet.length);
+        const randomQuote = questionSet[randomIndex];
+        questionSet.splice(randomIndex, 1);
+
+        scrambledQuestionSet.push(randomQuote);
+    }
+
+    return scrambledQuestionSet;
+}
+
+
+// generates a question set in the DOM
+function generateQuestionSet(questionSet) {
+    console.log("Question Set:", questionSet);
+
+    for (quote of questionSet) {
+        $('body').append(`<p>${quote.text}</p>`);
+        $('body').append(`<p>    - ${quote.author}</p>`);
+
+        // generate heart button
+        // const saveButton = $('<button>');
+        // saveButton.text('Save');
+        // saveButton.on('click', handleSaveQuoteButtonClick);
+        // $('body').append(saveButton);
+    }
+}
+
+
+function startNewQuestion() {
+    // if enough quotes are loaded for 1 new question
+    const enoughQuotesLoaded = (kanyeQuotes.length >= 1) && (otherQuotes.length >= numberOfOtherQuotes);
+    if (enoughQuotesLoaded) {
+        // stop waiting for loading
+        if (questionLoadInterval) { 
+            clearInterval(questionLoadInterval);
+            questionLoadInterval = null;
+        }
+
+        // generate a new question set
+        const questionSet = generateQuestionSetArray();
+        generateQuestionSet(questionSet);
+        currentQuestion++;
+    }
+
+    // if more time is needed to load quotes and waiting has not begun
+    else if (!questionLoadInterval) {
+        // begin rechecking if qutoes are loaded on interval
+        console.log("Begin waiting!")
+        questionLoadInterval = setInterval(startNewQuestion, waitForQuestionLoadTime);
+    }
+
+    else {
+        console.log("Wait for load...");
+    }
+}
+
+
+// begins a new game
+function startGame() {
+    currentQuestion = 0;
+    startNewQuestion();
+}
+
+
+// begins fetching quotes from APIs
+function beginFetchingQuotes() {
+    fetchOtherQuotes();
     fetchKanyeQuotes();
+}
+
+
+// executed one time once page loads
+function init() {
+    beginFetchingQuotes();
+    startGame();
 }
 
 
